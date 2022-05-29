835be6fe3103f836535fca892c9e66f17719957e
Bahaddin <bahaa.lashin@gmail.com>
2022-05-29 02:13:09 +0200

    job-model1

--- job/models.py
+++ job/models.py
@@ -1,3 +1,24 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPES = (
+    ('Full-Time','Full Time'),
+    ('Part-Time','Part-Time')
+)
+
+class Job(models.Model):
+    title = models.CharField(max_length=255,default=None,null=True)
+    # location
+    job_type = models.CharField(max_length=15,choices=JOB_TYPES,default='')
+    description = models.TextField(max_length=1000,null=True)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.FloatField(default=10.0)
+    # category
+    experience = models.IntegerField(default=1)
+
+
+    def __str__(self) -> str:
+        return self.title
